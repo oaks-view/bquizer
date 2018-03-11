dfe7056484018e0d1df9608426476aac7e0034a3
Moses igbuku <captainoak@gmail.com>
2018-03-11 09:31:51 +0100

    before removing awesome alert

--- App/Containers/QuizScreen.js
+++ App/Containers/QuizScreen.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, ImageBackground, Image } from 'react-native';
+import { View, ImageBackground, Image, Alert } from 'react-native';
 import { Text, Container, Content, Header, Body, Left, Right, Title, Footer, FooterTab, Button, H3, Thumbnail } from 'native-base';
 import styles from './Styles/QuizScreenStyles';
 import Icon from 'react-native-vector-icons/FontAwesome'
 import images from '../Themes/Images';
 import BackgroundImage from '../Components/BackgroundImage';
 import { Colors } from '../Themes/Colors';
-// import FIcon from 'react-native-vector-icons/FontAwesome'
+import AwesomeAlert from 'react-native-awesome-alerts';
 
 class QuizScreen extends React.Component {
     constructor(props) {
@@ -15,30 +15,95 @@ class QuizScreen extends React.Component {
         this.question = 'Who played Mirandas boyfriend robert in the movie, cloes art?';
 
         this.state = {
-            selectedAnswer: ''
+            selectedAnswer: { isCorrect: false },
+            showAlert: false,
+            alertTitle: '',
+            alertMessage: '',
+            currentQuiz: {
+                question: '',
+                answers: []
+            },
+            quizIndex: 0
         }
-
-        this.answers = [
-            { value: 'Will Smith', isCorrect: false, option: 'A' },
-            { value: 'Jason Statham', isCorrect: false, option: 'B' },
-            { value: 'Jackie Chan', isCorrect: false, option: 'C' }
-        ]
 
         this.renderOptionsBtn = this.renderOptionsBtn.bind(this);
     }
 
+    componentDidMount() {
+        this.setQuizSession();
+
+        this.setAlertFeedback();
+    }
+
+    setQuizSession() {
+        let quizz = [
+            {
+                question: 'Who was the first man to ever walk the earth?',
+                answers: [
+                    { value: 'Will Smith', isCorrect: false, option: 'A' },
+                    { value: 'Jason Statham', isCorrect: false, option: 'B' },
+                    { value: 'Jackie Chan', isCorrect: false, option: 'C' },
+                    { value: 'Adam', isCorrect: true, option: 'D' }
+                ]
+            },
+            {
+                question: 'I am th way, the truth and the life. Who said this?',
+                answers: [
+                    { value: 'Apostle Paul', isCorrect: false, option: 'A' },
+                    { value: 'Jesus Christ', isCorrect: true, option: 'B' },
+                    { value: 'John Smith', isCorrect: false, option: 'C' }
+                ]
+            }
+        ];
+
+        this.setState((prevState) => {
+            return {
+                currentQuiz: quizz[prevState.quizIndex]
+            }
+        });
+    }
+
+    showAlert = () => {
+        this.setState({
+            showAlert: true
+        });
+    };
+
+    hideAlert = () => {
+        this.setState({
+            showAlert: false
+        });
+    };
+
     renderOptionsBtn() {
-        return this.answers.map(answer => {
-            return (
-                <Button style={{ backgroundColor: Colors.darkBrownTransparent, marginBottom: 10}} key={answer.option} onPress={() => {
-                    this.setState({ selectedAnswer: answer})
-                }}>
-                    <Text uppercase={false} style={[this.state.selectedAnswer.option === answer.option && styles.selectedOption]}>
-                        {`${answer.option}. ${answer.value}`}
-                    </Text>
-                </Button>
-            )
-        })
+        let answers = this.state.currentQuiz.answers;
+
+        if (answers) {
+            return answers.map(answer => {
+                return (
+                    <Button style={{ backgroundColor: Colors.darkBrownTransparent, marginBottom: 10 }} key={answer.option} onPress={() => {
+                        this.setState({ selectedAnswer: answer })
+                    }}>
+                        <Text uppercase={false} style={[this.state.selectedAnswer.option === answer.option && styles.selectedOption]}>
+                            {`${answer.option}. ${answer.value}`}
+                        </Text>
+                    </Button>
+                )
+            })
+        }
+    }
+
+    setAlertFeedback() {
+        let answerCorrect = this.state.selectedAnswer.isCorrect;
+
+        this.alertTitle = answerCorrect ? '!Correct' : '!!!Wrong';
+
+        this.alertMessage = answerCorrect ? 'You got it right. Good Job'
+            : `Wrong Answer! the correct answer is `;
+
+        this.state.alertTitleStyle = answerCorrect ? { color: 'green' } : { color: 'red' };
+
+        this.state.alertMessageStyle = answerCorrect ? { color: 'green' } : { color: 'red' };
     }
 
     render() {
@@ -53,17 +118,57 @@ class QuizScreen extends React.Component {
                 <View style={{ flex: 1 }}>
                     <View style={[styles.dashboardMain, { flex: 1 }]}>
                         <View style={[styles.question, { flex: 4 }]}>
-                            <Text style={{ color: Colors.white, fontSize: 20 }}>{this.question}</Text>
+                            <Text style={{ color: Colors.white, fontSize: 20 }}>{this.state.currentQuiz.question}</Text>
                         </View>
                         <View style={[styles.options, { flex: 4 }]}>
                             {this.renderOptionsBtn()}
                         </View>
-                        <View style={{flex: 2, justifyContent: 'center', alignItems: 'center'}}>
-                            <Button rounded style={{alignSelf: 'center', justifyContent: 'center', alignItems:'center', backgroundColor: Colors.tomatoFaded, minWidth: 150}}>
-                                <Text style={{color: Colors.silver, fontSize: 20, alignItems: 'center', fontWeight: 'bold', borderColor: Colors.tomato, textAlign: 'center'}}>NEXT</Text>
+                        <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
+                            <Button onPress={() => { this.showAlert() }} rounded style={{ alignSelf: 'center', justifyContent: 'center', alignItems: 'center', backgroundColor: Colors.tomatoFaded, minWidth: 150 }}>
+                                <Text style={{ color: Colors.silver, fontSize: 20, alignItems: 'center', fontWeight: 'bold', borderColor: Colors.tomato, textAlign: 'center' }}>NEXT</Text>
                             </Button>
                         </View>
                     </View>
+                    {/* <AwesomeAlert
+                        show={this.state.showAlert}
+                        showProgress={false}
+                        title={this.state.alertTitle}
+                        message={this.state.alertMessage}
+                        closeOnTouchOutside={true}
+                        closeOnHardwareBackPress={false}
+                        showCancelButton={!this.state.selectedAnswer.isCorrect}
+                        showConfirmButton={this.state.selectedAnswer.isCorrect}
+                        cancelText="Next Question"
+                        confirmText="Next Question"
+                        confirmButtonColor="#DD6B55"
+                        onCancelPressed={() => {
+                            this.hideAlert();
+                        }}
+                        onConfirmPressed={() => {
+                            this.hideAlert();
+                        }}
+                    /> */}
+
+                    <AwesomeAlert
+                        show={this.state.showAlert}
+                        showProgress={false}
+                        title={this.state.alertTitle}
+                        message="I have a message for you!"
+                        closeOnTouchOutside={true}
+                        closeOnHardwareBackPress={false}
+                        showCancelButton={true}
+                        showConfirmButton={true}
+                        cancelText="No, cancel"
+                        confirmText="Yes, delete it"
+                        confirmButtonColor="#DD6B55"
+                        onCancelPressed={() => {
+                            this.hideAlert();
+                        }}
+                        onConfirmPressed={() => {
+                            this.hideAlert();
+                        }}
+                    />
+
                 </View>
                 <Footer>
                     <FooterTab style={styles.footerTab}>
