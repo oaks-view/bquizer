989c35573cdd98b95c86ccb41d33ea7001e4b0b4
Moses Igbuku <captainoak@gmail.com>
2026-05-13 17:55:54 +0200

    Create translations.ts

---
+++ i18n/en/translations.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1" language="de_DE" sourcelanguage="en">
+  <context>
+    <name>App</name>
+
+    <message>
+      <source>Welcome back</source>
+      <translation>Willkommen zurück</translation>
+    </message>
+
+    <message>
+      <source>Continue where you left off</source>
+      <translation>Mach dort weiter, wo du aufgehört hast</translation>
+    </message>
+
+    <message>
+      <source>Save</source>
+      <translation>Speichern</translation>
+    </message>
+
+    <message>
+      <source>Cancel</source>
+      <translation>Abbrechen</translation>
+    </message>
+
+    <message>
+      <source>Delete</source>
+      <translation>Löschen</translation>
+    </message>
+
+    <message>
+      <source>Something went wrong. Please check your connection.</source>
+      <translation>Etwas ist schiefgelaufen. Bitte überprüfe deine Verbindung.</translation>
+    </message>
+
+    <message>
+      <source>This field is required.</source>
+      <translation>Dieses Feld ist erforderlich.</translation>
+    </message>
+
+    <message>
+      <source>Your profile</source>
+      <translation>Dein Profil</translation>
+    </message>
+
+    <message>
+      <source>Edit profile</source>
+      <translation>Profil bearbeiten</translation>
+    </message>
+
+    <message>
+      <source>Settings</source>
+      <translation>Einstellungen</translation>
+    </message>
+
+    <message>
+      <source>Language</source>
+      <translation>Sprache</translation>
+    </message>
+
+    <message>
+      <source>Are you sure you want to log out?</source>
+      <translation>Möchtest du dich wirklich abmelden?</translation>
+    </message>
+
+  </context>
+</TS>
