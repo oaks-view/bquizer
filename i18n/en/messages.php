92e29b8b3dd36eafd6eeabdedd5be91747f58a51
Moses Igbuku <captainoak@gmail.com>
2026-05-13 22:06:54 +0200

    Create messages.php

---
+++ i18n/en/messages.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'app' => [
+        'home' => [
+            'title' => 'Welcome back',
+            'subtitle' => 'Continue where you left off',
+        ],
+
+        'button' => [
+            'save' => 'Save',
+            'cancel' => 'Cancel',
+            'delete' => 'Delete',
+        ],
+
+        'error' => [
+            'network' => 'Something went wrong. Please check your connection.',
+            'required_field' => 'This field is required.',
+        ],
+
+        'profile' => [
+            'title' => 'Your profile',
+            'edit' => 'Edit profile',
+        ],
+
+        'settings' => [
+            'title' => 'Settings',
+            'language' => 'Language',
+        ],
+
+        'logout' => [
+            'confirm' => 'Are you sure you want to log out?',
+        ],
+
+        'items' => [
+            'count' => '{0} You have no items|{1} You have one item|[2,*] You have :count items',
+        ],
+    ],
+];
